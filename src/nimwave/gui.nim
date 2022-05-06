c2f77859d414acba218d3c8ea6ec249a25465a5a
oakes <zach@sekao.net>
2022-05-06 17:46:52 +0200

    Remove input modules

--- src/nimwave/gui.nim
+++ src/nimwave/gui.nim
@@ -1,4 +1,4 @@
-import paranim/opengl, paranim/glm
+import paranim/opengl, paranim/glfw, paranim/glm
 import paranim/gl, paranim/gl/uniforms, paranim/gl/attributes
 from paranim/gl/entities import crop, color
 import paratext, paratext/gl/text
@@ -232,6 +232,61 @@ const
   brightBlueColor* = glm.vec4(103f/255f, 118f/255f, 246f/255f, 1f)
   brightMagentaColor* = glm.vec4(238f/255f, 131f/255f, 248f/255f, 1f)
   brightCyanColor* = glm.vec4(141f/255f, 250f/255f, 253f/255f, 1f)
+
+  glfwToIllwaveKey* =
+    {GLFWKey.Backspace: iw.Key.Backspace,
+     GLFWKey.Delete: iw.Key.Delete,
+     GLFWKey.Tab: iw.Key.Tab,
+     GLFWKey.Enter: iw.Key.Enter,
+     GLFWKey.Escape: iw.Key.Escape,
+     GLFWKey.Up: iw.Key.Up,
+     GLFWKey.Down: iw.Key.Down,
+     GLFWKey.Left: iw.Key.Left,
+     GLFWKey.Right: iw.Key.Right,
+     GLFWKey.Home: iw.Key.Home,
+     GLFWKey.End: iw.Key.End,
+     GLFWKey.PageUp: iw.Key.PageUp,
+     GLFWKey.PageDown: iw.Key.PageDown,
+     GLFWKey.Insert: iw.Key.Insert,
+     }.toTable
+  glfwToIllwaveCtrlKey* =
+    {GLFWKey.A: iw.Key.CtrlA,
+     GLFWKey.B: iw.Key.CtrlB,
+     GLFWKey.C: iw.Key.CtrlC,
+     GLFWKey.D: iw.Key.CtrlD,
+     GLFWKey.E: iw.Key.CtrlE,
+     GLFWKey.F: iw.Key.CtrlF,
+     GLFWKey.G: iw.Key.CtrlG,
+     GLFWKey.H: iw.Key.CtrlH,
+     GLFWKey.I: iw.Key.Tab,
+     GLFWKey.J: iw.Key.CtrlJ,
+     GLFWKey.K: iw.Key.CtrlK,
+     GLFWKey.L: iw.Key.CtrlL,
+     GLFWKey.M: iw.Key.Enter,
+     GLFWKey.N: iw.Key.CtrlN,
+     GLFWKey.O: iw.Key.CtrlO,
+     GLFWKey.P: iw.Key.CtrlP,
+     GLFWKey.Q: iw.Key.CtrlQ,
+     GLFWKey.R: iw.Key.CtrlR,
+     GLFWKey.S: iw.Key.CtrlS,
+     GLFWKey.T: iw.Key.CtrlT,
+     GLFWKey.U: iw.Key.CtrlU,
+     GLFWKey.V: iw.Key.CtrlV,
+     GLFWKey.W: iw.Key.CtrlW,
+     GLFWKey.X: iw.Key.CtrlX,
+     GLFWKey.Y: iw.Key.CtrlY,
+     GLFWKey.Z: iw.Key.CtrlZ,
+     GLFWKey.Backslash: iw.Key.CtrlBackslash,
+     GLFWKey.RightBracket: iw.Key.CtrlRightBracket,
+     }.toTable
+  glfwToIllwaveMouseButton* =
+    {GLFWMouseButton.Button1: iw.MouseButton.mbLeft,
+     GLFWMouseButton.Button2: iw.MouseButton.mbRight,
+     }.toTable
+  glfwToIllwaveMouseAction* =
+    {GLFWPress: iw.MouseButtonAction.mbaPressed,
+     GLFWRelease: iw.MouseButtonAction.mbaReleased,
+     }.toTable
 
 proc fgColorToVec4(ch: iw.TerminalChar, defaultColor: glm.Vec4[GLfloat]): glm.Vec4[GLFloat] =
   result =
