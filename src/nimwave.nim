542097c50d4120a08953ba6653faa4da751b3c43
oakes <zach@sekao.net>
2022-06-14 18:41:53 +0200

    Add scroll and text components

--- src/nimwave.nim
+++ src/nimwave.nim
@@ -156,9 +156,130 @@ proc renderHbox[T](ctx: var Context[T], node: JsonNode) =
 proc renderVbox[T](ctx: var Context[T], node: JsonNode) =
   renderBox(ctx, node, Vertical)
 
+type
+  ScrollState* = object
+    scrollX*: int
+    scrollY*: int
+
+proc renderScroll*[T](ctx: var Context[T], node: JsonNode, state: ref ScrollState) =
+  let
+    width = iw.width(ctx.tb)
+    height = iw.height(ctx.tb)
+    boundsWidth =
+      if "grow-x" in node and node["grow-x"].bval:
+        -1
+      else:
+        width
+    boundsHeight =
+      if "grow-y" in node and node["grow-y"].bval:
+        -1
+      else:
+        height
+    bounds = (0, 0, boundsWidth, boundsHeight)
+  var ctx = slice(ctx, state[].scrollX, state[].scrollY, width, height, bounds)
+  render(ctx, %* node["child"])
+  if "change-scroll-x" in node:
+    state[].scrollX += node["change-scroll-x"].num.int
+    let minX = width - iw.width(ctx.tb)
+    if minX < 0:
+      state[].scrollX = state[].scrollX.clamp(minX, 0)
+    else:
+      state[].scrollX = 0
+  if "change-scroll-y" in node:
+    state[].scrollY += node["change-scroll-y"].num.int
+    let minY = height - iw.height(ctx.tb)
+    if minY < 0:
+      state[].scrollY = state[].scrollY.clamp(minY, 0)
+    else:
+      state[].scrollY = 0
+
+proc mountScroll*[T](ctx: var Context[T], node: JsonNode, state: ref ScrollState): RenderProc[T] =
+  return
+    proc (ctx: var Context[T], node: JsonNode) =
+      renderScroll(ctx, node, state)
+
+proc mountScroll*[T](ctx: var Context[T], node: JsonNode): RenderProc[T] =
+  var state = new ScrollState
+  return mountScroll(ctx, node, state)
+
+type
+  TextState* = object
+    text*: string
+    cursorX*: int
+
+proc renderText*[T](ctx: var Context[T], node: JsonNode, state: ref TextState, scrollState: ref ScrollState) =
+  let
+    key = if "key" in node: iw.Key(node["key"].num.int) else: iw.Key.None
+    rune = if "rune" in node: Rune(node["rune"].num.int) else: Rune(0)
+  case key:
+  of iw.Key.Backspace:
+    if state[].cursorX > 0:
+      let
+        line = state[].text.toRunes
+        x = state[].cursorX - 1
+        newLine = $line[0 ..< x] & $line[x + 1 ..< line.len]
+      state[].text = newLine
+      state[].cursorX -= 1
+  of iw.Key.Delete:
+    if state[].cursorX < state[].text.runeLen:
+      let
+        line = state[].text.toRunes
+        newLine = $line[0 ..< state[].cursorX] & $line[state[].cursorX + 1 ..< line.len]
+      state[].text = newLine
+  of iw.Key.Left:
+    state[].cursorX -= 1
+    if state[].cursorX < 0:
+      state[].cursorX = 0
+  of iw.Key.Right:
+    state[].cursorX += 1
+    if state[].cursorX > state[].text.runeLen:
+      state[].cursorX = state[].text.runeLen
+  of iw.Key.Home:
+    state[].cursorX = 0
+  of iw.Key.End:
+    state[].cursorX = state[].text.runeLen
+  else:
+    discard
+  if rune.ord >= 32:
+    let
+      line = state[].text.toRunes
+      before = line[0 ..< state[].cursorX]
+      after = line[state[].cursorX ..< line.len]
+    state[].text = $before & $rune & $after
+    state[].cursorX += 1
+  # create scroll component
+  proc mountTextScroll(ctx: var Context[T], node: JsonNode): RenderProc[T] =
+    return mountScroll(ctx, node, scrollState)
+  ctx.statefulComponents["text-scroll"] = mountTextScroll
+  # update scroll position
+  let cursorXDiff = scrollState[].scrollX + state[].cursorX
+  if cursorXDiff >= iw.width(ctx.tb) - 1:
+    scrollState[].scrollX = iw.width(ctx.tb) - 1 - state[].cursorX
+  elif cursorXDiff < 0:
+    scrollState[].scrollX = 0 - state[].cursorX
+  # render
+  ctx = slice(ctx, 0, 0, iw.width(ctx.tb), 1)
+  render(ctx, %* {
+    "type": "text-scroll",
+    "id": "text-scroll",
+    "child": state[].text,
+  })
+  if "show-cursor" in node and node["show-cursor"].bval:
+    var cell = ctx.tb[scrollState[].scrollX + state[].cursorX, 0]
+    cell.bg = iw.bgYellow
+    cell.fg = iw.fgBlack
+    ctx.tb[scrollState[].scrollX + state[].cursorX, 0] = cell
+
+proc mountText*[T](ctx: var Context[T], node: JsonNode, state: ref TextState): RenderProc[T] =
+  var scrollState = new ScrollState
+  return
+    proc (ctx: var Context[T], node: JsonNode) =
+      renderText(ctx, node, state, scrollState)
+
 proc initContext*[T](): Context[T] =
   result = Context[T]()
   new result.mountedComponents
   result.components["nimwave.hbox"] = renderHbox[T]
   result.components["nimwave.vbox"] = renderVbox[T]
+  result.statefulComponents["nimwave.scroll"] = mountScroll[T]
 
