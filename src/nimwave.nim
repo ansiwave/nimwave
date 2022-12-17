28727746fc714cadcd7892449aaa30075b43f76e
oakes <zach@sekao.net>
2022-12-16 22:00:47 -0500

    Start work on moving from json nodes to methods

--- src/nimwave.nim
+++ src/nimwave.nim
@@ -1,20 +1,17 @@
 from illwave as iw import nil
-import tables, sets, json, unicode
+import tables, sets, unicode
 from nimwave/tui import nil
-from sequtils import nil
 from ansiutils/codes import nil
 
 type
-  MountProc*[T] = proc (ctx: var Context[T], node: JsonNode): RenderProc[T]
-  RenderProc*[T] = proc (ctx: var Context[T], node: JsonNode)
   Context*[T] = object
     tb*: iw.TerminalBuffer
-    ids: ref HashSet[seq[string]]
-    idPath: seq[string]
-    components*: Table[string, RenderProc[T]]
-    statefulComponents*: Table[string, MountProc[T]]
-    mountedComponents: ref Table[seq[string], RenderProc[T]]
+    ids*: ref HashSet[seq[string]]
+    idPath*: seq[string]
+    mountedComponents*: ref Table[seq[string], Component]
     data*: T
+  Component* = ref object of RootObj
+    id*: string
 
 proc slice*[T](ctx: Context[T], x, y: int, width, height: Natural): Context[T] =
   result = ctx
@@ -24,191 +21,13 @@ proc slice*[T](ctx: Context[T], x, y: int, width, height: Natural, bounds: tuple
   result = ctx
   result.tb = iw.slice(ctx.tb, x, y, width, height, bounds)
 
-proc renderComponent[T](ctx: var Context[T], node: JsonNode) =
-  if "type" notin node:
-    raise newException(Exception, "'type' required:\n" & $node)
-  let cmd = node["type"].str
-  var idPath: seq[string]
-  if "id" in node:
-    if node["id"].kind != JString:
-      raise newException(Exception, "id must be a string")
-    let id = node["id"].str
-    ctx.idPath.add(id)
-    if ctx.idPath in ctx.ids[]:
-      raise newException(Exception, "id already exists somewhere else in the tree: " & $ctx.idPath)
-    ctx.ids[].incl(ctx.idPath)
-    idPath = ctx.idPath
-  if cmd in ctx.components:
-    let f = ctx.components[cmd]
-    f(ctx, node)
-  elif idPath.len > 0 and idPath in ctx.mountedComponents:
-    let f = ctx.mountedComponents[idPath]
-    f(ctx, node)
-  elif cmd in ctx.statefulComponents:
-    if idPath.len == 0:
-      raise newException(Exception, "'id' required for stateful component:\n" & $node)
-    let m = ctx.statefulComponents[cmd]
-    let f = m(ctx, node)
-    if f == nil:
-      raise newException(Exception, "Stateful component did not return a render proc: " & cmd)
-    ctx.mountedComponents[idPath] = f
-    f(ctx, node)
-  else:
-    raise newException(Exception, "Component not found: " & cmd)
+proc all*(comps: varargs[Component]): seq[Component] =
+  for comp in comps:
+    result.add(comp)
 
-proc render*[T](ctx: var Context[T], node: JsonNode) =
-  if ctx.tb.buf == nil:
-    raise newException(Exception, "The `tb` field must be set in the context object")
-  if ctx.ids == nil:
-    new ctx.ids
-    render(ctx, node)
-    # unmount any components that weren't in the tree
-    for idPath in sequtils.toSeq(ctx.mountedComponents[].keys):
-      if idPath notin ctx.ids[]:
-        ctx.mountedComponents[].del(idPath)
-    # reset id fields
-    ctx.ids = nil
-    ctx.idPath = @[]
-    return
-  case node.kind:
-  of JString:
-    ctx = slice(ctx, 0, 0, codes.stripCodes(node.str).runeLen, 1)
-    try:
-      tui.write(ctx.tb, 0, 0, node.str)
-    except Exception as ex:
-      when defined(release):
-        discard
-      else:
-        raise ex
-  of JObject:
-    renderComponent(ctx, node)
-  of JArray:
-    for elem in node.elems:
-      render(ctx, elem)
-  else:
-    raise newException(Exception, "Invalid value:\n" & $node)
+# edit text
 
-proc render*[T](ctx: var Context[T], text: string) =
-  render(ctx, % text)
-
-proc flatten(nodes: seq[JsonNode], flatNodes: var seq[JsonNode]) =
-  for node in nodes:
-    if node.kind == JArray:
-      if node.elems.len > 0:
-        flatten(node.elems, flatNodes)
-    else:
-      flatNodes.add(node)
-
-proc flatten(nodes: seq[JsonNode]): seq[JsonNode] =
-  flatten(nodes, result)
-
-type
-  Direction = enum
-    Vertical, Horizontal,
-
-proc renderBox[T](ctx: var Context[T], node: JsonNode, direction: Direction) =
-  var
-    xStart = 0
-    yStart = 0
-  if "border" in node:
-    case node["border"].str:
-    of "single", "double", "none":
-      xStart = 1
-      yStart = 1
-  let children = if "children" in node: flatten(node["children"].elems) else: @[]
-  if children.len > 0:
-    case direction:
-    of Horizontal:
-      var
-        x = xStart
-        remainingWidth = iw.width(ctx.tb).int
-        remainingChildren = children.len
-        maxHeight = iw.height(ctx.tb)
-      for child in children:
-        let initialWidth = int(remainingWidth / remainingChildren)
-        var childContext = slice(ctx, x, yStart, max(0, initialWidth - (xStart * 2)), max(0, iw.height(ctx.tb) - (yStart * 2)))
-        render(childContext, child)
-        let actualWidth = iw.width(childContext.tb)
-        x += actualWidth
-        remainingWidth -= actualWidth
-        remainingChildren -= 1
-        maxHeight = max(maxHeight, iw.height(childContext.tb)+(yStart*2))
-      ctx = slice(ctx, 0, 0, x+xStart, maxHeight)
-    of Vertical:
-      var
-        y = yStart
-        remainingHeight = iw.height(ctx.tb).int
-        remainingChildren = children.len
-        maxWidth = iw.width(ctx.tb)
-      for child in children:
-        let initialHeight = int(remainingHeight / remainingChildren)
-        var childContext = slice(ctx, xStart, y, max(0, iw.width(ctx.tb) - (xStart * 2)), max(0, initialHeight - (yStart * 2)))
-        render(childContext, child)
-        let actualHeight = iw.height(childContext.tb)
-        y += actualHeight
-        remainingHeight -= actualHeight
-        remainingChildren -= 1
-        maxWidth = max(maxWidth, iw.width(childContext.tb)+(xStart*2))
-      ctx = slice(ctx, 0, 0, maxWidth, y+yStart)
-  if "border" in node:
-    case node["border"].str:
-    of "single":
-      iw.drawRect(ctx.tb, 0, 0, iw.width(ctx.tb)-1, iw.height(ctx.tb)-1)
-    of "double":
-      iw.drawRect(ctx.tb, 0, 0, iw.width(ctx.tb)-1, iw.height(ctx.tb)-1, doubleStyle = true)
-
-proc renderHBox*[T](ctx: var Context[T], node: JsonNode) =
-  renderBox(ctx, node, Horizontal)
-
-proc renderVBox*[T](ctx: var Context[T], node: JsonNode) =
-  renderBox(ctx, node, Vertical)
-
-type
-  ScrollState* = object
-    scrollX*: int
-    scrollY*: int
-
-proc renderScroll*[T](ctx: var Context[T], node: JsonNode, state: ref ScrollState) =
-  let
-    width = iw.width(ctx.tb)
-    height = iw.height(ctx.tb)
-    boundsWidth =
-      if "grow-x" in node and node["grow-x"].bval:
-        -1
-      else:
-        width
-    boundsHeight =
-      if "grow-y" in node and node["grow-y"].bval:
-        -1
-      else:
-        height
-    bounds = (0, 0, boundsWidth, boundsHeight)
-  var ctx = slice(ctx, state[].scrollX, state[].scrollY, width, height, bounds)
-  render(ctx, %* node["child"])
-  if "change-scroll-x" in node:
-    state[].scrollX += node["change-scroll-x"].num.int
-    let minX = width - iw.width(ctx.tb)
-    if minX < 0:
-      state[].scrollX = state[].scrollX.clamp(minX, 0)
-    else:
-      state[].scrollX = 0
-  if "change-scroll-y" in node:
-    state[].scrollY += node["change-scroll-y"].num.int
-    let minY = height - iw.height(ctx.tb)
-    if minY < 0:
-      state[].scrollY = state[].scrollY.clamp(minY, 0)
-    else:
-      state[].scrollY = 0
-
-proc mountScroll*[T](ctx: var Context[T], node: JsonNode, state: ref ScrollState): RenderProc[T] =
-  return
-    proc (ctx: var Context[T], node: JsonNode) =
-      renderScroll(ctx, node, state)
-
-proc mountScroll*[T](ctx: var Context[T], node: JsonNode): RenderProc[T] =
-  var state = new ScrollState
-  return mountScroll(ctx, node, state)
-
+#[
 type
   TextState* = object
     text*: string
@@ -284,11 +103,9 @@ proc mountText*[T](ctx: var Context[T], node: JsonNode, state: ref TextState): R
   return
     proc (ctx: var Context[T], node: JsonNode) =
       renderText(ctx, node, state, scrollState)
+]#
 
 proc initContext*[T](): Context[T] =
   result = Context[T]()
   new result.mountedComponents
-  result.components["nimwave.hbox"] = renderHBox[T]
-  result.components["nimwave.vbox"] = renderVBox[T]
-  result.statefulComponents["nimwave.scroll"] = mountScroll[T]
 
---
+++ src/nimwave/prelude.nim
@@ -0,0 +1,172 @@
+from nimwave import nil
+from sequtils import nil
+from ansiutils/codes import nil
+from tables import nil
+from sets import nil
+from nimwave/tui import nil
+from illwave as iw import nil
+
+method mount*(node: nimwave.Component, ctx: var nimwave.Context[State]) {.base, locks: "unknown".} =
+  discard
+
+method render*(node: nimwave.Component, ctx: var nimwave.Context[State]) {.base, locks: "unknown".} =
+  if node.id != "":
+    ctx.idPath.add(node.id)
+
+method unmount*(node: nimwave.Component, ctx: var nimwave.Context[State]) {.base, locks: "unknown".} =
+  discard
+
+proc renderRoot*(node: nimwave.Component, ctx: var nimwave.Context[State]) =
+  if ctx.tb.buf == nil:
+    raise newException(Exception, "The `tb` field must be set in the context object")
+  new ctx.ids
+  render(node, ctx)
+  # unmount any components that weren't in the tree
+  for idPath in sequtils.toSeq(ctx.mountedComponents[].keys):
+    if not sets.contains(ctx.ids[], idPath):
+      let comp = ctx.mountedComponents[idPath]
+      unmount(comp, ctx)
+      ctx.mountedComponents[].del(idPath)
+  # reset id fields
+  ctx.ids = nil
+  ctx.idPath = @[]
+
+proc getMounted*[T](node: T, ctx: var nimwave.Context[State]): T =
+  if node.id == "":
+    raise newException(Exception, "Node has no id")
+  elif ctx.idPath.len == 0 or ctx.idPath[^1] != node.id:
+    raise newException(Exception, "You must call the base method first! You can do it like this:\nprocCall render(nimwave.Component(node), ctx)")
+  if sets.contains(ctx.ids[], ctx.idPath):
+    raise newException(Exception, "id already exists somewhere else in the tree: " & $ctx.idPath)
+  sets.incl(ctx.ids[], ctx.idPath)
+  if not tables.contains(ctx.mountedComponents, ctx.idPath):
+    mount(node, ctx)
+    ctx.mountedComponents[ctx.idPath] = node
+    return node
+  else:
+    return cast[T](ctx.mountedComponents[ctx.idPath])
+
+# text
+
+type
+  Text* = ref object of nimwave.Component
+    content*: string
+
+method render*(node: Text, ctx: var nimwave.Context[State]) =
+  procCall render(nimwave.Component(node), ctx)
+  ctx = nimwave.slice(ctx, 0, 0, codes.stripCodes(node.content).runeLen, 1)
+  tui.write(ctx.tb, 0, 0, node.content)
+
+# box
+
+type
+  Direction* {.pure.} = enum
+    Vertical, Horizontal,
+  Border* {.pure.} = enum
+    None, Single, Double, Hidden,
+  Box* = ref object of nimwave.Component
+    direction*: Direction
+    border*: Border
+    children*: seq[nimwave.Component]
+
+method render*(node: Box, ctx: var nimwave.Context[State]) =
+  procCall render(nimwave.Component(node), ctx)
+  var
+    xStart = 0
+    yStart = 0
+  case node.border:
+  of None:
+    discard
+  of Single, Double, Hidden:
+    xStart = 1
+    yStart = 1
+  if node.children.len > 0:
+    case node.direction:
+    of Horizontal:
+      var
+        x = xStart
+        remainingWidth = iw.width(ctx.tb).int
+        remainingChildren = node.children.len
+        maxHeight = iw.height(ctx.tb)
+      for child in node.children:
+        let initialWidth = int(remainingWidth / remainingChildren)
+        var childContext = nimwave.slice(ctx, x, yStart, max(0, initialWidth - (xStart * 2)), max(0, iw.height(ctx.tb) - (yStart * 2)))
+        render(child, childContext)
+        let actualWidth = iw.width(childContext.tb)
+        x += actualWidth
+        remainingWidth -= actualWidth
+        remainingChildren -= 1
+        maxHeight = max(maxHeight, iw.height(childContext.tb)+(yStart*2))
+      ctx = nimwave.slice(ctx, 0, 0, x+xStart, maxHeight)
+    of Vertical:
+      var
+        y = yStart
+        remainingHeight = iw.height(ctx.tb).int
+        remainingChildren = node.children.len
+        maxWidth = iw.width(ctx.tb)
+      for child in node.children:
+        let initialHeight = int(remainingHeight / remainingChildren)
+        var childContext = nimwave.slice(ctx, xStart, y, max(0, iw.width(ctx.tb) - (xStart * 2)), max(0, initialHeight - (yStart * 2)))
+        render(child, childContext)
+        let actualHeight = iw.height(childContext.tb)
+        y += actualHeight
+        remainingHeight -= actualHeight
+        remainingChildren -= 1
+        maxWidth = max(maxWidth, iw.width(childContext.tb)+(xStart*2))
+      ctx = nimwave.slice(ctx, 0, 0, maxWidth, y+yStart)
+  case node.border:
+  of Single:
+    iw.drawRect(ctx.tb, 0, 0, iw.width(ctx.tb)-1, iw.height(ctx.tb)-1)
+  of Double:
+    iw.drawRect(ctx.tb, 0, 0, iw.width(ctx.tb)-1, iw.height(ctx.tb)-1, doubleStyle = true)
+  else:
+    discard
+
+# scroll
+
+type
+  Scroll* = ref object of nimwave.Component
+    scrollX*: int
+    scrollY*: int
+    changeScrollX*: int
+    changeScrollY*: int
+    growX*: bool
+    growY*: bool
+    child*: nimwave.Component
+
+method render*(node: Scroll, ctx: var nimwave.Context[State]) =
+  procCall render(nimwave.Component(node), ctx)
+  let
+    changeScrollX = node.changeScrollX
+    changeScrollY = node.changeScrollY
+    mNode = getMounted(node, ctx)
+  let
+    width = iw.width(ctx.tb)
+    height = iw.height(ctx.tb)
+    boundsWidth =
+      if mNode.growX:
+        -1
+      else:
+        width
+    boundsHeight =
+      if mNode.growY:
+        -1
+      else:
+        height
+    bounds = (0, 0, boundsWidth, boundsHeight)
+  var ctx = nimwave.slice(ctx, mNode.scrollX, mNode.scrollY, width, height, bounds)
+  render(mNode.child, ctx)
+  if changeScrollX != 0:
+    mNode.scrollX += changeScrollX
+    let minX = width - iw.width(ctx.tb)
+    if minX < 0:
+      mNode.scrollX = mNode.scrollX.clamp(minX, 0)
+    else:
+      mNode.scrollX = 0
+  if changeScrollY != 0:
+    mNode.scrollY += changeScrollY
+    let minY = height - iw.height(ctx.tb)
+    if minY < 0:
+      mNode.scrollY = mNode.scrollY.clamp(minY, 0)
+    else:
+      mNode.scrollY = 0
