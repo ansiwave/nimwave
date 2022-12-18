2def2023b1de3ee06cd501c82bbb094301214fdd
oakes <zach@sekao.net>
2022-12-18 10:09:39 -0500

    Add mount/unmount test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,11 +1,34 @@
 import unittest
 
+from illwave as iw import nil
+from nimwave as nw import nil
+
 type
   State = object
+  CustomNode = ref object of nw.Node
 
 include nimwave/prelude
 
-from nimwave as nw import nil
+var
+  mounted = false
+  unmounted = false
 
-test "can initialize prelude":
-  discard nw.initContext[State]()
+method mount*(node: CustomNode, ctx: var nw.Context[State]) =
+  mounted = true
+
+method render*(node: CustomNode, ctx: var nw.Context[State]) =
+  discard getMounted(node, ctx)
+
+method unmount*(node: CustomNode, ctx: var nw.Context[State]) =
+  unmounted = true
+
+test "mount and unmount":
+  var ctx = nw.initContext[State]()
+  ctx.tb = iw.initTerminalBuffer(0, 0)
+  renderRoot(CustomNode(id: "test"), ctx)
+  check mounted
+  check not unmounted
+  renderRoot(CustomNode(id: "test"), ctx)
+  check not unmounted
+  renderRoot(nw.Text(str: "hi"), ctx)
+  check unmounted
